9ec056fac35696e872b7c132cb2b3cfd4f1a6570
Louis Bedard <foutchy@hotmail.com>
2015-12-16 17:30:34 -0500

    fix config to match url

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ config = {
     // When running Ghost in the wild, use the production environment.
     // Configure your URL and mail settings here
     production: {
-        url: 'http://minutesdepot-blog.herokuapp.com',
+        url: 'http://blog.minutesdepot.com',
         fileStorage: false,
         mail: {  
             transport: 'SMTP',
